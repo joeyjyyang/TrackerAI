d683569617c6cff61220e36c7d6d8f909814f339
yukunchen113 <cheny113@mcmaster.ca>
2019-01-26 17:55:27 -0500

    test json

---
+++ test_json.py
@@ -0,0 +1,300 @@
+test_route = {
+   "geocoded_waypoints" : [
+      {
+         "geocoder_status" : "OK",
+         "place_id" : "ChIJwSr3RKyELIgRHAFDYeoAnjk",
+         "types" : [ "establishment", "point_of_interest", "university" ]
+      },
+      {
+         "geocoder_status" : "OK",
+         "place_id" : "ChIJmzrzi9Y0K4gRgXUc3sTY7RU",
+         "types" : [ "establishment", "point_of_interest", "premise" ]
+      }
+   ],
+   "routes" : [
+      {
+         "bounds" : {
+            "northeast" : {
+               "lat" : 43.641677,
+               "lng" : -79.38665709999999
+            },
+            "southwest" : {
+               "lat" : 43.2553954,
+               "lng" : -79.919539
+            }
+         },
+         "copyrights" : "Map data ©2019 Google",
+         "legs" : [
+            {
+               "distance" : {
+                  "text" : "67.2 km",
+                  "value" : 67172
+               },
+               "duration" : {
+                  "text" : "48 mins",
+                  "value" : 2907
+               },
+               "end_address" : "301 Front St W, Toronto, ON M5V 2T6, Canada",
+               "end_location" : {
+                  "lat" : 43.641677,
+                  "lng" : -79.38665709999999
+               },
+               "start_address" : "1280 Main St W, Hamilton, ON L8S 4L8, Canada",
+               "start_location" : {
+                  "lat" : 43.2581671,
+                  "lng" : -79.91915499999999
+               },
+               "steps" : [
+                  {
+                     "distance" : {
+                        "text" : "29 m",
+                        "value" : 29
+                     },
+                     "duration" : {
+                        "text" : "1 min",
+                        "value" : 7
+                     },
+                     "end_location" : {
+                        "lat" : 43.258361,
+                        "lng" : -79.91891409999999
+                     },
+                     "html_instructions" : "Head \u003cb\u003enorth-east\u003c/b\u003e on \u003cb\u003eUniversity Ave\u003c/b\u003e",
+                     "polyline" : {
+                        "points" : "qz_gGtexfNGEGIGGMW"
+                     },
+                     "start_location" : {
+                        "lat" : 43.2581671,
+                        "lng" : -79.91915499999999
+                     },
+                     "travel_mode" : "DRIVING"
+                  },
+                  {
+                     "distance" : {
+                        "text" : "89 m",
+                        "value" : 89
+                     },
+                     "duration" : {
+                        "text" : "1 min",
+                        "value" : 47
+                     },
+                     "end_location" : {
+                        "lat" : 43.2580925,
+                        "lng" : -79.9193703
+                     },
+                     "html_instructions" : "Make a \u003cb\u003eU-turn\u003c/b\u003e",
+                     "maneuver" : "uturn-left",
+                     "polyline" : {
+                        "points" : "w{_gGddxfNG[QHF^JVJNNPFDFDNF"
+                     },
+                     "start_location" : {
+                        "lat" : 43.258361,
+                        "lng" : -79.91891409999999
+                     },
+                     "travel_mode" : "DRIVING"
+                  },
+                  {
+                     "distance" : {
+                        "text" : "1.0 km",
+                        "value" : 1034
+                     },
+                     "duration" : {
+                        "text" : "2 mins",
+                        "value" : 130
+                     },
+                     "end_location" : {
+                        "lat" : 43.2584626,
+                        "lng" : -79.90740169999999
+                     },
+                     "html_instructions" : "Turn \u003cb\u003eleft\u003c/b\u003e onto \u003cb\u003eMain St W\u003c/b\u003e/\u003cb\u003eHamilton Regional Rd 8\u003c/b\u003e",
+                     "maneuver" : "turn-left",
+                     "polyline" : {
+                        "points" : "az_gG`gxfNf@P`@N?_@AyDCaEC{C?WEgDAuDEcEA{@C_B?_AAOC{AASA}A?EAIAEMYEy@E}@IoAYwEQ}CE_AAQGcAC_@Ck@"
+                     },
+                     "start_location" : {
+                        "lat" : 43.2580925,
+                        "lng" : -79.9193703
+                     },
+                     "travel_mode" : "DRIVING"
+                  },
+                  {
+                     "distance" : {
+                        "text" : "0.7 km",
+                        "value" : 728
+                     },
+                     "duration" : {
+                        "text" : "1 min",
+                        "value" : 51
+                     },
+                     "end_location" : {
+                        "lat" : 43.2571459,
+                        "lng" : -79.90342489999999
+                     },
+                     "html_instructions" : "Turn \u003cb\u003eright\u003c/b\u003e  onto the \u003cb\u003eOntario 6 N\u003c/b\u003e/\u003cb\u003eToronto\u003c/b\u003e/\u003cb\u003eOntario 403 E\u003c/b\u003e slip road",
+                     "polyline" : {
+                        "points" : "k|_gGf|ufNFIBEBEFKDIBC@ABC@ABADAF?J?d@J~@Rz@RbATh@Lb@FT@F@FAF?TCPETKTOXWDEDGJOHSHO@EBIDMDUDQ?E@IBY@S?S?SAc@ASCQCMCOCKEOGMEIEMEGEIIKWYGKACCG}A{ACAU[[c@Ye@U]Q]IQECAAKC"
+                     },
+                     "start_location" : {
+                        "lat" : 43.2584626,
+                        "lng" : -79.90740169999999
+                     },
+                     "travel_mode" : "DRIVING"
+                  },
+                  {
+                     "distance" : {
+                        "text" : "32.7 km",
+                        "value" : 32702
+                     },
+                     "duration" : {
+                        "text" : "20 mins",
+                        "value" : 1170
+                     },
+                     "end_location" : {
+                        "lat" : 43.4814184,
+                        "lng" : -79.6725079
+                     },
+                     "html_instructions" : "Merge onto \u003cb\u003eON-403 E\u003c/b\u003e",
+                     "maneuver" : "merge",
+                     "polyline" : {
+                        "points" : "et_gGjcufNi@qAc@oAYcAOi@WqAOcAMy@Gm@My@WoBUqBWsBWmBSaB]oCWyAIc@G[IYI]IWIYK[IUKYYk@MWKSOWOUMS]e@SUKOQOSSOOOMUQUOQKSMSKOIWKOG[KQGWGSEMCWE]Ec@E_@C[Cm@Cm@E_ACm@EuAGqBImEQqCMgCOe@Cu@CyDQg@Cg@CiAGg@Aa@Ck@Ck@Ce@Am@?a@Ae@?a@@q@?{@Bu@Be@@iAF{BHkAFcBF}AH}@Dc@@k@BaADq@Bg@@]@[?YA_@A_@CWASEQCq@Mu@Ow@Oi@I_@GSEm@Iw@Iy@I}@Gc@Cw@Ee@CO?c@?[@_@@OBWBYBYFg@J_@J]La@NSHg@X_@VUNYV[XSPWXMLuA~AY\\}AhB_AfA_AfAu@z@m@p@_@^c@`@_Ap@_@TWPMHUHa@PYJUFa@L_@H_@D_@F_@Bc@DU?]?Y?c@C[C_@Ek@IYG]Go@Sw@[[M[OUKOMSOUSg@c@a@a@Y]_@e@_@i@}BoDgEsH}@wA}@yAw@oAm@aAa@q@y@qAw@qASa@Q[Yi@Se@Uk@Uo@W{@Uw@I[g@}Ba@mBk@iCWgAo@wCs@kDCGcAsEKg@Oq@K_@Kg@Mc@K_@K]Oe@Ma@Ws@Si@Qc@Qa@Qc@Wi@We@S]e@{@k@eAc@{@o@mAo@kAS_@?Ac@{@a@u@_@q@i@eAq@oAc@{@s@qA_@w@m@gA_@u@{@_BeAsBu@uAo@oAc@y@_@q@y@yAi@aA_AgBiAuBw@{Aw@}Ac@w@m@gAc@{@c@w@]k@m@}@y@mAqAoBq@cAk@}@i@}@q@eAaAyAqDuFk@y@k@}@aBeCsAsB}@sA{@kAi@u@i@q@c@i@o@s@u@y@c@g@i@i@cAiAwB{BiCoC{A_BkBoBqBwB{BaC}A_BaBgBqCwC}AcByB}ByA_B}AaBs@u@kAqAaAcAiAmAaBgBkBoBoCuCsByBmCsCsCyCoAsAq@s@qAwA_CeCsAuAuAyA[]}@}@MOiAkAw@}@i@k@]g@k@o@y@_Au@}@eAoAw@aA{@cA}@gAs@u@_AcAy@{@eAgAcAiA[[]_@MOk@s@SWa@i@S[i@u@U_@Yg@[g@Q]Yi@Ym@Ym@Yo@Oa@Ys@Uo@Ss@Oi@Sw@Ou@O{@Ku@Io@Iw@I_AKmAG{@Ek@Ek@Ee@Iu@Ks@Mu@Oy@S_AGYYaAIWUo@K[KUEMEIu@aBw@sA]e@o@y@}@aAg@c@i@a@OM_@UCC{B{Aw@i@e@_@g@c@_@a@[_@WY[e@_@i@Yc@e@}@Wi@Ug@eAcC]y@AGYs@c@oAU[_@aAu@qBk@{AaBiEmB_FgC}G_D}Iq@gBQi@Si@sFkOm@}AsAaDgAaCk@mAg@aAgAyBaAeBeAcBm@}@}@qAeAyAoA}AwAcB}AiBq@s@mR_TY[aCuCc@s@s@o@Y]wGqHgDqDcD_EeSgUeBmBwRsTOSaKiLmD_EWYeHgIwMeOcAeAkAuAcHkIq@u@yN{OyRsTgOuP_FsF_@e@iMqNg@g@}HwI_AeAu@{@kCsCiDwDmC{Ca@c@iBuBaAgAu@u@sCcDsCaDsA{AyDwEwA}AwA{AqAwAy@aAk@k@SWo@q@q@u@iCuCeBmB{GsHkF}F{BgCqAwAkC_DqA}A{AgBaKwLyDkEs@u@qE}EyAcB}@cAiAmAiAmAeBcBwB{ByB}BsAwAiBoBqAwAgBsB}EsFqB{B_BkBiBqB{NaPsEcFqEcFwAcByCgDmBwByCmDyEoFuCeDcDsDSUuAaBoB{BiIoJoD_EaKmLoA{AoLcNoC_D_CmCkCyCgHkIUWaBmByAaBgCwCa@e@oDcEoCeDkEcFcDsD]_@gBuBsH{IeBwBQU]a@aPuQmGeHs@u@aAcAa@a@q@y@u@y@gAoAiAmA_AaAi@m@k@q@u@{@i@q@kA}AcAwAaCaDgAwACCu@y@_CcCwAuAiFsFy@y@e@e@cBoBeCoC}AgB_@c@_@a@IIqAyAkDuDyA_BkBsBeAiA_CeCq@w@iAmAuA_BcAkAeAiAwA_B_BkBAAo@u@cAgAgCsCgAeAyAsAm@g@qAeA_BiAaAq@o@a@cAi@o@]oCsA]O]M}@]}@[}@Yg@O_AWi@Mk@Mi@McAQOCaAOwBWs@GwAIkDMoBA}@@aA@iBFsDJwCHwEL"
+                     },
+                     "start_location" : {
+                        "lat" : 43.2571459,
+                        "lng" : -79.90342489999999
+                     },
+                     "travel_mode" : "DRIVING"
+                  },
+                  {
+                     "distance" : {
+                        "text" : "17.6 km",
+                        "value" : 17639
+                     },
+                     "duration" : {
+                        "text" : "11 mins",
+                        "value" : 633
+                     },
+                     "end_location" : {
+                        "lat" : 43.6096921,
+                        "lng" : -79.55429239999999
+                     },
+                     "html_instructions" : "Keep \u003cb\u003eleft\u003c/b\u003e at the fork to continue on \u003cb\u003eQueen Elizabeth Way\u003c/b\u003e, follow signs for \u003cb\u003eToronto\u003c/b\u003e",
+                     "maneuver" : "fork-left",
+                     "polyline" : {
+                        "points" : "{mkhGd`heNom@`B_MZiBB}@BcCH}AFs@@{AB_BBk@Bq@@q@BoEHaHRe@@kA?{@AyAG{@Km@KKAe@Ks@Ok@Om@Su@WaAc@}@e@q@a@k@_@UOYSUUqAmAkCsCgLoM{CkDcFqFoC_DaCkC{LeNiDyD_CkCaCoC{HyIsEgFaDqDkCuCiAoAuA}AqB}B[]oAuAqFkGuB{BaGyGoE_FkCwCqAyAUWaBiBeAiAqD}D{@cAeBkBqB}BsA{AQUaEuEqDaEe@i@wGwHyH_JiE_F}DmEgDwDgCsCcCuCaEsE}BgCuB_C{AcBgAmA{EqFsEiFmEaFgCsCwA}AuA_Bo@s@eAkAs@w@uA_B_@a@aAgAc@i@_@e@QW[a@c@k@{@qAe@w@wAaCy@{Aw@_Bs@}As@cBO[Uo@a@gAUo@c@oAqAoEYw@Uo@_@_A[y@M[Yk@i@iAw@uAo@cAe@u@a@k@EGy@eAq@u@iAiAw@w@c@]SQu@m@a@Yi@_@e@YUQwCoB}@k@o@a@YS}AaAy@k@y@k@g@_@y@i@c@[g@[oBqAe@[WO}B_BoAy@mBoAaAo@yBuAuBwAsA}@y@m@e@_@_@Yw@q@i@e@_@]a@a@[Yc@e@aAeAgAqAOQcHsHmAyAu@y@wA_Bk@m@SUc@i@MOQUSSw@y@q@o@mAoAgEsEwCeDCAiCyCgBmBAAqB}BgAmA}AgB[_@qDaEmBuBoAyAyBcCgAqAqHiIq@w@iBuBsDaEqFkGo@s@gBqBcAmAi@m@sA}AqB}B}AcBaNiOW]kGaHoHgI_AaAgAmAoD{DCCuHqI{AeBwNaPoGeHeGyG}CkDwGyH_BgBgAsAkCyCgBsBEGwEoFiBcCeAgACCoAyAi@m@_@a@_@c@sIwJ}H_JwA_BuBeCSWSU"
+                     },
+                     "start_location" : {
+                        "lat" : 43.4814184,
+                        "lng" : -79.6725079
+                     },
+                     "travel_mode" : "DRIVING"
+                  },
+                  {
+                     "distance" : {
+                        "text" : "13.6 km",
+                        "value" : 13627
+                     },
+                     "duration" : {
+                        "text" : "10 mins",
+                        "value" : 599
+                     },
+                     "end_location" : {
+                        "lat" : 43.6378358,
+                        "lng" : -79.39859779999999
+                     },
+                     "html_instructions" : "Keep \u003cb\u003eleft\u003c/b\u003e to continue on \u003cb\u003eGardiner Expy E\u003c/b\u003e",
+                     "maneuver" : "keep-left",
+                     "polyline" : {
+                        "points" : "qodiGh}pdNGAIGMOkBwBcBmBgAoAq@w@GIy@aAs@cAc@q@[e@KSUa@a@u@[o@e@gAw@uBOe@Uu@_@uAIe@Qo@Mu@GYGYKs@Ks@Ii@Gc@E]KaAWuDKaBY}DImAy@kLGiAEq@Cc@AQ?M?I@Ik@_GSsBSgB?CcAeJWuBCOkAqKeB_OMeAMeAGK?AACe@eEQuAm@oFi@yEg@iE}@{HcAwI_@kDs@aGe@iEm@eFaAsIg@qEmFyd@wBcRa@gD[gCUmBqAuJY}Bg@oDCOAI?IAE@EACoBwMIc@m@_Ea@oCiBwLe@}CQgAe@_DIe@q@sEu@yEm@}DM}@COGY{@}FAAe@_Du@{Dg@eDUaBEWGk@SgAQeAWkAOq@e@{Ac@sAg@iAg@aAi@_Aa@q@c@o@sDiEUY_@k@_@i@}@sAs@kAKQq@gAoByCaBsCq@qAm@qAWk@]w@]w@_AyBYs@k@iAu@{Ao@mAm@mAe@}@q@sAq@uAa@w@EMOYKWSg@u@sBY{@c@mA{@kCi@aBs@_C[cAc@yA[kAa@_BOq@Me@Kk@WoAQ{@Mm@My@QeA[sBS{AUsBOyAIaAIu@IcAK{AK{BI}ACwAEuBAiA?kB?yBB{ADkBBsABoAFcBDaAHcBJiBLkBJsAL{ARqBTuBTmBPmAD_@ZoBBOV}Ab@_CReAZuAVcAPw@V{@Ru@\\gATw@lM}`@n@oBfAiDJWZ}@l@oBPi@Tw@J]Jc@H]Nu@Jg@Hi@F]D]Dc@D[BSB[@UBYBWBe@@_@@S?UBgA?]?W?a@AS?UAo@Ae@Cc@A[CWC_@EYCQCQIk@Gi@Ko@Kg@AIAG?C?ACQESAEAEAKEUG[SgAYyAy@qEScAk@{C]gBm@iDWuAWsA[eB]iBKm@c@yB]oBO{@EUEUG]Ig@g@mCk@}CcBiJ{AkIk@cDM}@WyAOeAMuASaDGqBCiB?{@?y@?yA?yAAy@A{@EmBEmBE_BG}A]kGc@yJAw@Aw@As@Ay@@aA?k@?o@BgA@w@FuEFqD"
+                     },
+                     "start_location" : {
+                        "lat" : 43.6096921,
+                        "lng" : -79.55429239999999
+                     },
+                     "travel_mode" : "DRIVING"
+                  },
+                  {
+                     "distance" : {
+                        "text" : "0.5 km",
+                        "value" : 514
+                     },
+                     "duration" : {
+                        "text" : "1 min",
+                        "value" : 59
+                     },
+                     "end_location" : {
+                        "lat" : 43.6385515,
+                        "lng" : -79.39237899999999
+                     },
+                     "html_instructions" : "Take the \u003cb\u003eSpadina Ave.\u003c/b\u003e exit",
+                     "maneuver" : "ramp-right",
+                     "polyline" : {
+                        "points" : "o_jiGfprcNHOBE?E@G?SBm@@q@?Y?c@Ac@Aa@Ci@Ee@Eg@O}AEWGw@I_ASoBEi@Gu@UeCE_@AWAICSAUEWC[COIm@Ge@"
+                     },
+                     "start_location" : {
+                        "lat" : 43.6378358,
+                        "lng" : -79.39859779999999
+                     },
+                     "travel_mode" : "DRIVING"
+                  },
+                  {
+                     "distance" : {
+                        "text" : "0.2 km",
+                        "value" : 245
+                     },
+                     "duration" : {
+                        "text" : "1 min",
+                        "value" : 72
+                     },
+                     "end_location" : {
+                        "lat" : 43.6405619,
+                        "lng" : -79.392866
+                     },
+                     "html_instructions" : "Turn \u003cb\u003eleft\u003c/b\u003e onto \u003cb\u003eLower Spadina Ave\u003c/b\u003e/\u003cb\u003eSpadina Ave\u003c/b\u003e\u003cdiv style=\"font-size:0.9em\"\u003eContinue to follow Spadina Ave\u003c/div\u003e",
+                     "maneuver" : "turn-left",
+                     "polyline" : {
+                        "points" : "}cjiGjiqcNG_@AM_A\\i@NUHSDc@Jc@HIB_@JaCj@"
+                     },
+                     "start_location" : {
+                        "lat" : 43.6385515,
+                        "lng" : -79.39237899999999
+                     },
+                     "travel_mode" : "DRIVING"
+                  },
+                  {
+                     "distance" : {
+                        "text" : "0.6 km",
+                        "value" : 565
+                     },
+                     "duration" : {
+                        "text" : "2 mins",
+                        "value" : 139
+                     },
+                     "end_location" : {
+                        "lat" : 43.641677,
+                        "lng" : -79.38665709999999
+                     },
+                     "html_instructions" : "Turn \u003cb\u003eright\u003c/b\u003e onto \u003cb\u003eBremner Blvd\u003c/b\u003e",
+                     "maneuver" : "turn-right",
+                     "polyline" : {
+                        "points" : "opjiGllqcNS_BKs@?YAM?I?I@K?M?K@WFc@JcAJmAPsBHcABW@K@I?K@K?G?O?M?C?M?E?EAM?SCSAIAKAE?CCMAEAEAGAEAGAGEMGUCEAGCEEKCICGGMEIACCCGGiAuACEAACCA?C?A?MOS[MQIKIMEICC"
+                     },
+                     "start_location" : {
+                        "lat" : 43.6405619,
+                        "lng" : -79.392866
+                     },
+                     "travel_mode" : "DRIVING"
+                  }
+               ],
+               "traffic_speed_entry" : [],
+               "via_waypoint" : []
+            }
+         ],
+         "overview_polyline" : {
+            "points" : "qz_gGtexfNe@o@G[QHRv@j@l@v@X`@N?_@E{JCsDO}QOmKO_@KwB{@eOQaDVa@NSREp@JzBf@fDl@l@Cf@Qn@g@`@q@Tm@Lw@DuAKwAUy@cA_B{BaC}AeCOUMEmAaDi@mB}@}FsAoKaByLe@wBi@cB_AqBkAgBeAiAkA_AiAo@uAg@qAWyGa@yXoAuNo@{CCeEH{Qt@gENsBA}AOiEw@yCa@sE[uBAaBL_Cl@}BjA_BrAuCdDsGtHmApAcBrA{Az@sBr@_CZqBDkCWw@OgBo@}Aw@sBiBy@cA}CyEeGkKuBiDaEwGsAmCyAoE_CqKkFaVq@iCiAeDmAsC}BeE}F{KqVce@qJoQuL{QkJsNwFiIsDiEiGwGq[y\\{S}Tcl@cn@kDaEmFoGmFeG_FiFkB{BuB{CaBwC}AmDsAaE_A_FcAoLs@yE_AsDs@oB{@kBuAyBmB{BqAeAoDcCeDoCoBiCmBsD_CyFy@kBuAsDcKcXwFyOaHiR{CcH}EoJwFmIwH_JgS{TeDiEmOqPiXg[}UaXwQwSm[i^uIaKsb@oe@qe@ai@a]m_@uJwKyNsPoOwPiVgXkK}L{PcSeGsGkHcIkLuLaOoPwX{ZwSsUu^cb@ww@e~@c^{a@uNmPiMkOwb@{e@_FoFoFcHcGwH{NgOiOwPiW_Y_QuRaDyC_CmBaD{BsBkA{FoCaFcB_EaAmG}@kCQ{GO}KVisAnDqZp@qB@uCI{Ce@cEkA_CiA}AaAwCgCs\\a_@is@ix@}[s^c^s`@_\\u^sl@qq@we@ki@uQkSwCoDaDwEqC}EkB}D{BwFeDgK_BeEkDoGgCoDwEuEuCyBsNkJuKkH{VsPgDqCaC_C}L}MgGaHwAeBkEmEqWqY}]y`@gY}[eWiYsSiU{s@gx@o`@gd@oDkEsA}A}LkN_P}Qs@o@iImJyCaE_BqCaAwBgA{CqAaFi@}Cc@_DiAwOqAuREmAqAeN_BoNmE}_@o@wEgJkx@aRc`BqD{XcQsjAkByLyB{MeAeGu@mCkA}CwCcFiEcF_AuAoDyFqEmH_BcDsCuGkDgH{FmLgCeHuDqLqBwHcA}EiAaHcAkJk@qIUuJHmLTiIn@mKlAqLv@mFjByJtAmFpPmh@|EsOn@eDX{BXqEBsCC{BK}BQ}Am@eEaI_c@cDoQ_Jig@g@_Da@wFKqICiHYyKaAeSCoBA{ELeKFqDHOBKF{BCcCkAuMs@qIWqBOeAAM_A\\_AXw@PoEdAa@{D@m@`AiLDwAIeBOy@c@uASc@yAgBKEq@}@]g@"
+         },
+         "summary" : "ON-403 E and Queen Elizabeth Way",
+         "warnings" : [],
+         "waypoint_order" : []
+      }
+   ],
+   "status" : "OK"
+}
---
+++ yukun_gmaps_test.py
@@ -0,0 +1,3 @@
+import test_json
+data = test_json.test_route
+steps = data["routes"]["legs"][0]["steps"]
