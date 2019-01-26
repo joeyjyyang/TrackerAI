d8883b40a693153c13ca66d7cd4d5af164da6d07
Preetham-J <j.preetham42@gmail.com>
2019-01-26 18:37:04 -0500

    Added more information

--- directions/directions_analytics.py
+++ directions/directions_analytics.py
@@ -4,14 +4,18 @@ from datetime import datetime
 
 gmaps = googlemaps.Client(key.key)
 
-# Request directions via driving
+# Request directions
 now = datetime.now()
 directions_result = gmaps.directions("McMaster University, Hamilton, ON",
                                      "CN Tower, Toronto, ON",
                                      mode="driving",
                                      departure_time=now)
 
-for route in directions_result[0]["legs"][0]["steps"]:
-    print("Lat, Long: " + str(route["start_location"]["lat"]) + ", " + str(route["start_location"]["lng"]))
+# Print direction, start and end latitude and longitude
+for step in directions_result[0]["legs"][0]["steps"]:
+    print("Direction: " + str(step["distance"]["text"]))
+    print("Start Lat/Long: " + str(step["start_location"]["lat"]) + "/" + str(step["start_location"]["lng"]))
+    print("End Lat/Long: " + str(step["end_location"]["lat"]) + "/" + str(step["end_location"]["lng"]))
+
 
 
