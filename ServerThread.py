d9586e8c96a68c9bea87604efa788c369aa8140c
bojanglesjoey <joey.yang1997@hotmail.com>
2019-01-27 00:17:44 -0500

    removed code methods; moving to server level instead

--- ServerThread.py
+++ ServerThread.py
@@ -1,5 +1,5 @@
 from Truck import Truck
-from Thread import Timer
+from threading import Timer
 
 class ServerThread: 
 	
@@ -30,37 +30,11 @@ class ServerThread:
 			#self.requestCode() -- called by server instead 
 			return False
 
-	# correspond to send request, while loop, is past time limit, is code recieved
-	def requestCode(self):
-		timer = Timer(self.timeLimit, self.sendAlert) #start timer
-		bFlag = True
-		while (bFlag):
-			bFlag = self.receiveCode(timer, code)	
-		self.verifyCode(code)
-
-	def receiveCode(self, timer, code):
-		if (code):
-			timer.cancel()
-			print("code received.")
-			#self.verifyCode(code)
-			return False 
-		else:
-			return True
-			
-	# corresponds to is code correct, update route
-	def verifyCode(self, code):
-		if (code == self.truck.code):
-			self.setRoute()
-		else:
-			self.sendAlert() 	
-
-	# corresponds to Alert
 	def sendAlert(self):
 		# prompt GUI to notify user if they want to request driver code
 		# this.parent.displayAlert
-	# for GUI
+	
 	def updateLocation(self):
-		
-	return
+		return
 		# returns current latitude and longitude
 		# this.parent.displayLocation
