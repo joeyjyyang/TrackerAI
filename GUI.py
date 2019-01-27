a5172881f7438872ea0a71d40e7c85f5ff9f5937
bojanglesjoey <joey.yang1997@hotmail.com>
2019-01-27 11:31:48 -0500

    GUI initialized by server thread

--- GUI.py
+++ GUI.py
@@ -1,40 +1,49 @@
 from tkinter import *
 
-def setSafeLabel():
-	alertLabel = Label(text=" SAFE ", height=5, width=15, bg="white").grid(row=0, column=0)  
+class GUI:
 
-def readServerParameters():
-	with open("serverParameters.txt", "r") as f:
-		alertFlag = f.read()
+	def __init__(self):
+		window = Tk()
+		window.title("Controller GUI")
+		labels = ['Truck ID', 'Start Location', 'Destination', 'Latitude', 'Longitude']
+		gridRow = 1
+		setSafeLabel()
+		alertButton = Button(text='Ignore Alert', relief=SUNKEN, height=4, width=12, command=setSafeLabel).grid(row=0, column=1)
+		for label in labels:
+			Label(text=label, relief=RIDGE, width=15).grid(row=gridRow, column=0)
+			Entry(relief=SUNKEN, width=15).grid(row=gridRow, column=1)
+			#Entry(relief=SUNKEN, width=15).grid(row=gridRow, column=1)
+			gridRow = gridRow + 1
+		readServerParameters()
+		window.mainloop()
+
+	def updateGUI(self, alert):
 		try:
-			if (int(alertFlag)):
+			if (int(alert)):
 				setAlertLabel()
 			else:
 				setSafeLabel()
 		except ValueError:
 			pass
-	window.after(1000, readServerParameters)			
-def setAlertLabel():
-	alertLabel = Label(text='ALERT', relief=RIDGE, height=5, width=15, bg="red").grid(row=0, column=0)
 
-window = Tk()
-window.title("Controller GUI")
+	def setSafeLabel(self):
+		alertLabel = Label(text=" SAFE ", height=5, width=15, bg="white").grid(row=0, column=0)  
 
-labels = ['Truck ID', 'Start Location', 'Destination', 'Latitude', 'Longitude']
+	'''def readServerParameters(self):
+		with open("serverParameters.txt", "r") as f:
+			alertFlag = f.read()
+			try:
+				if (int(alertFlag)):
+					setAlertLabel()
+				else:
+					setSafeLabel()
+			except ValueError:
+				pass
+		window.after(1000, readServerParameters)'''		
 
-gridRow = 1
+	def setAlertLabel(self):
+		alertLabel = Label(text='ALERT', relief=RIDGE, height=5, width=15, bg="red").grid(row=0, column=0)
 
-setSafeLabel()
 
-alertButton = Button(text='Ignore Alert', relief=SUNKEN, height=4, width=12, command=setSafeLabel).grid(row=0, column=1)
-
-for label in labels:
-	Label(text=label, relief=RIDGE, width=15).grid(row=gridRow, column=0)
-	Entry(relief=SUNKEN, width=15).grid(row=gridRow, column=1)
-	gridRow = gridRow + 1
-
-readServerParameters()
-
-window.mainloop()
 
 
