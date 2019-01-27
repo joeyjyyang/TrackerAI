9c19326fe4db72dfb93356d0c85e6a673cab6a05
bojanglesjoey <joey.yang1997@hotmail.com>
2019-01-26 23:39:35 -0500

    pushing ServerThread method implementions

--- ServerThread.py
+++ ServerThread.py
@@ -1,43 +1,59 @@
 from Truck import Truck
+from Thread import Timer
 
-#0 Server sends JSON to GUI (which is always listening for new missions)
-#1 GUI class instantites new Truck in a new thread, with data from JSON
-#2 GUI class adds truck ID to drop down menu to update GUI to this truck
-#2.1 GUI passes in default timeLimit and has option to change it
-#2.2 default timeLimit globally configurable
 class ServerThread: 
-	# corresponds to Start
-	def __init__(self, timeLimit, ID, startLatitude, startLongitude, destination):
-		self.truck = Truck(timeLimit, ID, startLatitude, startLongitude, destination)
-		# call setRoute (or in GUI?)
+	
+	def __init__(self, timeLimit, ID, code, startLocation, destination, latitude, longitude):
+		self.truck = Truck(ID, code, startLocation, destination, latitude, longitude)
+		self.timeLimit = timeLimit
+		# call setRoute and API (or in GUI?)
 
-	# corresponds to Start
 	def setRoute(self):
+		return
 		# instantiate/updates API with startLocation and destination
 		# when API returns that it is finished, start receiveLocation and updateLocation()
 
-	# corresponds to is data received
-	def receiveLocation(self):
-		# While loop until data is received
+	def receiveLocation(self, latitude, longitude):
+		while True:
+			if (latitude && longitude):
+				#call update location
+				self.verifyLocation(latitude, longitude)
+				
+# While loop until data is received
 		# once data is received, update gpsData and call verifyLocation
 
-	# corresponds to is correct
 	def verifyLocation(self):
 		# call API instance to check route
 		# returns okay or not okay
-		# if okay call receiveLocation (i.e. go back to loop)
-		# if not okay call requestCode
+		check = True #set to return value of API call
+		if (check):
+			self.receiveLocation()
+		else:	
+			self.requestCode()
 
 	# correspond to send request, while loop, is past time limit, is code recieved
 	def requestCode(self):
-		# start timer
-		# if timer exceeds timeLimit, call sendAlert()
-		# otherwise, if code is received before timeLimit, call verifyCode
+		timer = Timer(self.timeLimit, self.sendAlert) #start timer
+		bFlag = True
+		while (bFlag):
+			bFlag = self.receiveCode(timer, code)	
+		self.verifyCode(code)
 
+	def receiveCode(self, timer, code):
+		if (code):
+			timer.cancel()
+			print("code received.")
+			#self.verifyCode(code)
+			return False 
+		else:
+			return True
+			
 	# corresponds to is code correct, update route
-	def verifyCode(self):
-		# if not correct sendAlert()
-		# if correct, call setRoute()
+	def verifyCode(self, code):
+		if (code == self.truck.code):
+			self.setRoute()
+		else:
+			self.sendAlert() 	
 
 	# corresponds to Alert
 	def sendAlert(self):
