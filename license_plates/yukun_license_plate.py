4806bda422136c76181f8a5425abbe5624a7ced3
yukunchen113 <cheny113@mcmaster.ca>
2019-01-27 01:17:30 -0500

    all

--- license_plates/yukun_license_plate.py
+++ license_plates/yukun_license_plate.py
@@ -1,16 +1,15 @@
 import requests
 import base64
 import json
-
+import keys
 # Sample image file is available at http://plates.openalpr.com/ea7the.jpg
-SECRET_KEY = 'sk_8177f61cdeef542eedc11a0e'
 def get_plate(image_path):
 	with open(image_path, 'rb') as image_file:
 	    img_base64 = base64.b64encode(image_file.read())
-	url = 'https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=us&secret_key=%s' % (SECRET_KEY)
+	url = 'https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=us&secret_key=%s' %(keys.lpr)
 	r = requests.post(url, data = img_base64)
 	return r.json()["results"][0]["plate"]
 
 
-test_image = 'license_plates/Solana_resort_villa_with_car.jpg'
+test_image = 'kd.jpg'
 print(get_plate(test_image))
