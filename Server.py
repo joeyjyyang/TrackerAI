586ef64dea8e453d9f247566ae999fd1aa823280
justinmcook <cookj1@mcmaster.ca>
2019-01-27 01:05:40 -0500

    Create most of Server.py

--- Server.py
+++ Server.py
@@ -1,9 +1,10 @@
 from threading import Thread
+from threading import Timer
 from ServerThread import ServerThread
 import json
 import random
 
-timeLimit = 30
+defaultTimeLimit = 30
 
 
 def main():
@@ -17,8 +18,39 @@ def main():
 
 
 def createMission(jsonData):
-    code = random.randint(1, 100000)
-    mission = ServerThread(timeLimit, jsonData["ID"], code, jsonData["startLocation"], jsonData["destination"], 0.0, 0.0)
+    driverCode = random.randint(1, 100000)
+    mission = ServerThread(defaultTimeLimit, jsonData["ID"], driverCode, jsonData["startLocation"], jsonData["destination"], 0.0, 0.0)
+    while True:
+        position = receiveLocation()
+        mission.setLatitude(position["latitude"])
+        mission.setLongitude(position["longitude"])
+        check = mission.verifyLocation()
+        if not check:
+            inputCode = requestCode(mission.getTimeLimit(), mission)
+            mission.verifyCode(inputCode)
+
+
+def receiveLocation():
+    position = {
+        "latitude": None,
+        "longitude": None
+    }
+    
+    return position
+
+
+def requestCode(missionTimeLimit, mission):
+    timer = Timer(missionTimeLimit, mission.sendAlert)
+    while not mission.alert:
+        inputCode = receiveCode()
+        if inputCode is not None:
+            timer.cancel()
+            return inputCode
+
+
+# TODO: Ping driver
+def receiveCode():
+    return input("\nPlease enter your code: ")
 
 
 if __name__ == "__main__":
