ecf0526e1665c5566e80c3324d89bf62eb6d8f41
bojanglesjoey <joey.yang1997@hotmail.com>
2019-01-26 20:47:54 -0500

    changed file name

--- ServerThread.py
+++ ServerThread.py
@@ -1,43 +1,49 @@
-from Truck import Truck
-from threading import Timer
+from truck.py import Truck
 
+#0 Server sends JSON to GUI (which is always listening for new missions)
+#1 GUI class instantites new Truck in a new thread, with data from JSON
+#2 GUI class adds truck ID to drop down menu to update GUI to this truck
+#2.1 GUI passes in default timeLimit and has option to change it
+#2.2 default timeLimit globally configurable
 class ServerThread: 
+	# corresponds to Start
+	def __init__(self, timeLimit, ID, startLatitude, startLongitude, destination):
+		self.truck = Truck(timeLimit, ID, startLatitude, startLongitude, destination)
+		# call setRoute (or in GUI?)
 
-	alert = False
-	
-	def __init__(self, timeLimit, ID, code, startLocation, destination, latitude, longitude):
-		self.truck = Truck(ID, code, startLocation, destination, latitude, longitude)
-		self.timeLimit = timeLimit
-		self.setRoute()
-
+	# corresponds to Start
 	def setRoute(self):
-		return
 		# instantiate/updates API with startLocation and destination
 		# when API returns that it is finished, start receiveLocation and updateLocation()
 
-	def setLatitude(self, latitude):
-		self.latitude = latitude
+	# corresponds to is data received
+	def receiveLocation(self):
+		# While loop until data is received
+		# once data is received, update gpsData and call verifyLocation
 
-	def setLongitude(self, longitude):
-		self.longitude = longitude
-
-	def getTimeLimit(self):
-		return self.timeLimit
-
+	# corresponds to is correct
 	def verifyLocation(self):
 		# call API instance to check route
 		# returns okay or not okay
-		check = True #set to return value of API call
-		if (check):
-			return True
-		else:	
-			return False
+		# if okay call receiveLocation (i.e. go back to loop)
+		# if not okay call requestCode
 
-	def verifyCode(self, inputCode):
-		if (inputCode == self.truck.code):
-			self.setRoute()
-		else:
-			self.sendAlert() 
+	# correspond to send request, while loop, is past time limit, is code recieved
+	def requestCode(self):
+		# start timer
+		# if timer exceeds timeLimit, call sendAlert()
+		# otherwise, if code is received before timeLimit, call verifyCode
 
+	# corresponds to is code correct, update route
+	def verifyCode(self):
+		# if not correct sendAlert()
+		# if correct, call setRoute()
+
+	# corresponds to Alert
 	def sendAlert(self):
-		alert = True
+		# prompt GUI to notify user if they want to request driver code
+		# this.parent.displayAlert
+	# for GUI
+	def updateLocation(self):
+		# returns current latitude and longitude
+		# this.parent.displayLocation
