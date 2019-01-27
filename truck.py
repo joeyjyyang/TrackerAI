f3883c83b9512a1ca7304116e91a0bce14751622
bojanglesjoey <joey.yang1997@hotmail.com>
2019-01-26 20:40:28 -0500

    class and function declarations and interfaces

---
+++ truck.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+@dataclass
+class Truck: 
+        ID: str
+        startLatitude: float
+        startLongitude: float
+        destination: str
+        latitude: float
+        longitude: float
+	
